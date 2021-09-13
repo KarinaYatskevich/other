0c8f63fd6c2aeb2fc5bd87a4b09c9456fe150d50
karina <karinav19817@gmail.com>
2021-09-13 22:30:55 +0300

    commit

---
+++ MyFirstProject/settings/index.js
@@ -0,0 +1,26 @@
+/* grab sliders on page */
+const sliders = document.querySelectorAll('input[type="range"]')
+
+/* take a slider element, return a percentage string for use in CSS */
+const rangeToPercent = slider => {
+    const max = slider.getAttribute('max') || 10;
+    const percent = slider.value / max * 100;
+
+    return `${parseInt(percent)}%`;
+};
+
+/* on page load, set the fill amount */
+sliders.forEach(slider => {
+    slider.style.setProperty('--track-fill', rangeToPercent(slider));
+
+  /* when a slider changes, update the fill prop */
+    slider.addEventListener('input', e => {
+        e.target.style.setProperty('--track-fill', rangeToPercent(e.target));
+    })
+})
+
+const form = document.querySelector('form')
+form.addEventListener('input', event => {
+    const formData = Object.fromEntries(new FormData(form));
+    console.table(formData);
+})
